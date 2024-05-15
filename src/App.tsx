35d962c53c53e61e2db8aa8f0d9141db3fc333aa
ZhenyaGoroh <zhenyagoroh@gmail.com>
2024-05-15 18:23:30 +0300

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Input from "./components/Input";
 function App() {
   const [results, setResults] = useState("");
   const [plans, setPlans] = useState("");
+  const [name, setName] = useState("");
 
   const gradientText = {
     background: "linear-gradient(to left, #9FCC2E, #52FFEE)",
@@ -23,6 +24,7 @@ function App() {
   return (
     <div className="w-full h-screen bg-black flex items-center justify-center">
       <div className="flex flex-col gap-2 w-80">
+        <Input value={name} onChange={(e) => setName(e.target.value)} />
         <div style={gradientText} className="font-extrabold text-4xl">Results:</div>
         <Input value={results} onChange={(e) => setResults(e.target.value)} />
         <div style={gradientText} className="font-extrabold text-4xl">Plans:</div>
--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Telegraf, Context, Markup } from "telegraf";
+import { Telegraf, Context } from "telegraf";
 import 'dotenv/config'
 
 interface MyContext extends Context {
@@ -10,38 +10,6 @@ const bot = new Telegraf<MyContext>(process.env.VITE_BOT_TOKEN as string);
 
 bot.start((ctx) => {
   ctx.reply("https://t.me/dailySync_bot/DailySync");
-});
-
-bot.command("quit", async (ctx) => {
-  // Explicit usage
-  await ctx.telegram.leaveChat(ctx.message.chat.id);
-
-  // Using context shortcut
-  await ctx.leaveChat();
-});
-
-bot.on('message', async (ctx) => {
-  await ctx.telegram.sendMessage(
-    ctx.message.chat.id,
-    `Hello ${ctx.from.username}`,
-    {
-      reply_markup: Markup.inlineKeyboard([
-        Markup.button.callback("Click me", "hey_there"),
-      ]).reply_markup,
-    }
-  );
-});
-
-bot.action("hey_there", async (ctx) => {
-  await ctx.reply("Hey Natasha!");
-});
-
-bot.on("callback_query", async (ctx) => {
-  // Explicit usage
-  await ctx.telegram.answerCbQuery(ctx.callbackQuery.id);
-
-  // Using context shortcut
-  await ctx.answerCbQuery();
 });
 
 bot.launch();
