b790fea9e8e517cb58036f8c8d709e9276aacfa5
ZhenyaGoroh <zhenyagoroh@gmail.com>
2024-05-15 18:25:01 +0300

    Added name to message

--- src/App.tsx
+++ src/App.tsx
@@ -13,23 +13,40 @@ function App() {
   };
 
   const sendMessage = async () => {
-    const chatId = '798698474';
+    const chatId = "798698474";
 
     const botToken = import.meta.env.VITE_BOT_TOKEN as string;
-    const text = `✅Results:\n${results}\n📅Plans:\n${plans}`;
+    const text = `✅Results:\n${results}\n📅Plans:\n${plans}\n\n${name}`;
 
-    await fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(text)}`);
+    await fetch(
+      `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(
+        text
+      )}`
+    );
   };
 
   return (
     <div className="w-full h-screen bg-black flex items-center justify-center">
       <div className="flex flex-col gap-2 w-80">
+        <div style={gradientText} className="font-extrabold text-4xl">
+          Name:
+        </div>
         <Input value={name} onChange={(e) => setName(e.target.value)} />
-        <div style={gradientText} className="font-extrabold text-4xl">Results:</div>
+        <div style={gradientText} className="font-extrabold text-4xl">
+          Results:
+        </div>
         <Input value={results} onChange={(e) => setResults(e.target.value)} />
-        <div style={gradientText} className="font-extrabold text-4xl">Plans:</div>
+        <div style={gradientText} className="font-extrabold text-4xl">
+          Plans:
+        </div>
         <Input value={plans} onChange={(e) => setPlans(e.target.value)} />
-        <button className="h-10 w-full p-10 bg-green flex items-center justify-center text-2xl font-bold rounded" disabled={results.length === 0 || plans.length === 0} onClick={sendMessage}>Send</button>
+        <button
+          className="h-10 w-full p-10 bg-green flex items-center justify-center text-2xl font-bold rounded"
+          disabled={results.length === 0 || plans.length === 0 || name.length === 0}
+          onClick={sendMessage}
+        >
+          Send
+        </button>
       </div>
     </div>
   );
